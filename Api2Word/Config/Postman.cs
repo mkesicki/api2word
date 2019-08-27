c247252f7f636a75ce40b9602a6aa047701a9bfd
mkesicki <marc_he_w@poczta.onet.pl>
2019-08-28 00:06:17 +0200

    Add Config parser for Postman

--- Api2Word/Config/Postman.cs
+++ Api2Word/Config/Postman.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using Api2Word;
+using YamlDotNet.RepresentationModel;
+using System.IO;
 
 namespace Api2Word.Config
 {
@@ -14,18 +15,57 @@ namespace Api2Word.Config
         public IFormatter Formatter { get; set; }
 
         public Dictionary<String, String> Authorization { get; set; }
+        public Dictionary<String, String> Config { get; set; }
 
-        public String Path { set { Path = value; } }
-        public String CollectionName { get { return CollectionName; }  set { CollectionName = value; } }
+        public String Path { get; set; }
+        public String CollectionName { get; set; }
 
         public Postman(String path, String name) {
             Path = path;
             CollectionName = name;
             Authorization = new Dictionary<String, String>();
+            Config = new Dictionary<String, String>();
             ReadConfig();
         }
 
         public void ReadConfig() {
+
+            System.Console.WriteLine("Read Config File: \"{0}\"", Path);
+
+            //read YAML
+            using (var reader = new StreamReader(Path))
+            {
+                // Load the stream
+                var yaml = new YamlStream();
+                yaml.Load(reader);
+                // List all the items
+                var mapping =
+               (YamlMappingNode)yaml.Documents[0].RootNode;
+
+                var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("config")];
+                foreach (YamlMappingNode item in items)
+                {
+                    Config.Add(item.Children.First().Key.ToString(), item.Children.First().Value.ToString());
+                }
+
+                items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("authorization")];
+                foreach (YamlMappingNode item in items)
+                {
+                    Authorization.Add(item.Children.First().Key.ToString(), item.Children.First().Value.ToString());
+                }
+
+                foreach (KeyValuePair<String, String> kvp in Config)
+                {
+                    System.Console.WriteLine("Config--{0}:{1}", kvp.Key, kvp.Value);
+                }
+
+                foreach (KeyValuePair<String, String> kvp in Authorization)
+                {
+                    System.Console.WriteLine("Auth--{0}:{1}", kvp.Key, kvp.Value);
+                }
+            }
+
+
         }
     }
 }
