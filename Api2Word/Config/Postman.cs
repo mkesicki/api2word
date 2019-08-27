1cf0faf328814618693afea21486cc31600f287f
mkesicki <marc_he_w@poczta.onet.pl>
2019-08-28 00:04:23 +0200

    Add Endpoint class

---
+++ Api2Word/Config/Postman.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Api2Word;
+
+namespace Api2Word.Config
+{
+   class Postman : IConfig
+    {
+        public String Url { get; set; }
+        public IParser Parser { get; set; }
+        public IFormatter Formatter { get; set; }
+
+        public Dictionary<String, String> Authorization { get; set; }
+
+        public String Path { set { Path = value; } }
+        public String CollectionName { get { return CollectionName; }  set { CollectionName = value; } }
+
+        public Postman(String path, String name) {
+            Path = path;
+            CollectionName = name;
+            Authorization = new Dictionary<String, String>();
+            ReadConfig();
+        }
+
+        public void ReadConfig() {
+        }
+    }
+}
---
+++ Api2Word/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api2Word
+{
+    public class Endpoint
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public String Url { get; set; }
+        public String Method { get; set; }
+        public String Request { get; set; }
+        public String Response { get; set; }
+        public String Headers { get; set; }
+        public Int16 StatusCode { get; set; }
+    }
+}
