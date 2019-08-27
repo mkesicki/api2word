ed104a4f828754e873ffbbf37fdfb763a7f21c75
mkesicki <marc_he_w@poczta.onet.pl>
2019-08-28 00:06:35 +0200

    Add console program class:

--- Api2Word/Program.cs
+++ Api2Word/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Api2Word
 {
     class Program
     {
-        static void Main(string[] args)
+        private static String configPath , collectionName, formatterName, collectionType;
+
+        static int Main(string[] args)
         {
+            if (args.Length < 3) {
+                System.Console.WriteLine("You need to pass 3 parameters: \n" +
+                    "collection type" +
+                    "path to config file \n" +
+                    "name of Collection to parse \n"
+                );
+
+                return 1;
+            }
+
+            collectionType = args[0].ToLower();
+            configPath = args[1];
+            collectionName = args[2];
+            
+
+            System.Console.WriteLine("Passed args: {0}, {1}, {2}", collectionType, configPath, collectionName);
+
+            if (!File.Exists(configPath)) {
+                System.Console.WriteLine("Invalid config file path.");
+
+                return 1;
+            }
+
+            collectionType = collectionType.First().ToString().ToUpper() + collectionType.Substring(1);
+
+            String objectType = "Api2Word.Config." + collectionType;
+            Type type = Type.GetType(objectType);
+            IConfig config = (IConfig)Activator.CreateInstance(type, configPath, collectionName);
+
+            return 0;
         }
     }
 }
