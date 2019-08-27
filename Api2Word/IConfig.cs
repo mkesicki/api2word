8f9241347d69b6178e6b1276847ac8f9f05a0066
mkesicki <marc_he_w@poczta.onet.pl>
2019-08-28 00:09:03 +0200

    Add interfaces

---
+++ Api2Word/IConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api2Word
+{
+    public interface IConfig
+    {
+        Dictionary<String, String> Authorization { get; set; }
+        Dictionary<String, String> Config { get; set; }
+
+        String Path { get;  set; }
+        String CollectionName { get; set; }
+        IParser Parser { get; set; }
+        IFormatter Formatter { get; set; }
+        String Url { get; set; }
+
+        void ReadConfig();
+       // void SetParser();
+//void SetFormatter();
+       // IFormatter GetFormatter();
+        //IParser GetParser();
+
+    }
+}
