21e582a70d0e01962b34314e929701c4e58f4804
mkesicki <marc_he_w@poczta.onet.pl>
2019-09-26 20:33:03 +0200

    Add Env files

---
+++ Api2Word/Parser/Env.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public class Value
+{
+    public bool Enabled { get; set; }
+    public string Key { get; set; }
+    public string value { get; set; }
+    public string Type { get; set; }
+}
+
+public class Environment
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public List<Value> Values { get; set; }
+}
+
+public class EnvironmentBody
+{
+    public Environment Environment { get; set; }
+}
---
+++ Api2Word/Parser/EnvInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class EnvInfo
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Owner { get; set; }
+    public string Uid { get; set; }
+}
+
+public class EnvList
+{
+    public List<EnvInfo> Environments { get; set; }
+}
